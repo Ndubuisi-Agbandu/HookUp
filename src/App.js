4a3de8942a6aedaffc9f3085fabd2cb30c8a22a4
Ndubuisi-Agbandu <ndubuisiagbandu98@gmail.com>
2020-02-10 20:07:16 +0100

    Started the Hookup App with react.js (Actually my first time using react.js) and i know its not looking good and pretty now.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'tachyons';
+import FacebookLogin from 'react-facebook-login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container-div">
+        <div className="flex flex-column items-center">
+          <h1>HookUp</h1>
+          <article className="mb4">HookUp with someone from any of your social media handles and go out on a date with them.</article>
+          <FacebookLogin
+            appId="1088597931155576"
+            autoLoad={true}
+            fields="name,email,picture"
+            onClick={componentClicked}
+            callback={responseFacebook} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
